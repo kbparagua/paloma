8d0d8bd9de32decbb098f03c1e9ad2aa8f63d52f
kbparagua <kb.paragua@gmail.com>
2013-08-27 15:47:18 +0800

    Rewriting core

--- vendor/assets/javascripts/paloma.js
+++ vendor/assets/javascripts/paloma.js
@@ -4,12 +4,44 @@ window.Paloma = window.Paloma || {};
 (function(P){
 
   // Initialize Containers
-  P.Resources = [];
+  P.Resources = {};
 
 
-  P.Resource = function(options){
+  P.execute = function(callback){
+    var resource = P.Resources[callback.resource];
+    if (resource == undefined){ return false; }
 
+    var action = resource[action];
+    if (action == undefined){ return false; }
+
+    action(callback.params);
   };
+
+
+
+  P.Resource = function(options){};
+
+
+  P.Resource.create = function(name, options){
+    options = defaultOptions(options);
+
+    var path = name.split('.'),
+        resource = name.pop(),
+        namespace = name.pop();
+
+    var constructor = function(){};
+
+    if (namespace != null){
+      P.Resources[namespace] = P.Resource[namespace] || {};
+      P.Resources[namespace][resource] = constructor;
+    }
+    else {
+      P.Resources[resource] = constructor;
+    }
+
+    return constructor;
+  };
+
 
 
   // Filters
@@ -18,14 +50,12 @@ window.Paloma = window.Paloma || {};
   };
 
 
-  P.Resource._defaultOptions = function(options){
+  var defaultOptions = function(options){
     var values = {};
     values['extend'] = options['extend'] != null ? options['extend'] : null;
 
     return values;
   };
-
-
 
 
 })(window.Paloma);
