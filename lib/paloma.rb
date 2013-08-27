dddadbda2dcd4770ae12f57bcd494f6b912754d1
kbparagua <kb.paragua@gmail.com>
2013-08-27 14:28:59 +0800

    Remove require ActionControllerFilters

--- lib/paloma.rb
+++ lib/paloma.rb
@@ -1,25 +1,25 @@
 module Paloma
   mattr_accessor :destination, :templates
-  
+
   def self.root
     @paloma_root ||= "#{File.dirname(__FILE__)}/../"
   end
-  
-  
+
+
   def self.index_js
     @index_js ||= "#{Paloma.destination}/index.js"
   end
-  
-  
+
+
   def self.destination
     @destination ||= 'app/assets/javascripts/paloma'
   end
-  
-  
+
+
   def self.templates
     @templates ||= "#{Paloma.root}/app/templates"
   end
-  
+
 end
 
 require 'action_controller/railtie'
@@ -29,7 +29,6 @@ require 'rails/generators'
 
 require 'paloma/generators/add_generator'
 require 'paloma/generators/setup_generator'
-require 'paloma/action_controller_filters'
 require 'paloma/action_controller_extension'
 require 'paloma/rails/controller_generator'
 require 'paloma/rails/engine'
