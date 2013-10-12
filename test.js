bf94d4cb921b76b605cf1b9d3a47811f6565e8af
kbparagua <kb.paragua@gmail.com>
2013-10-12 08:49:45 +0800

    Test syntax

---
+++ test.js
@@ -0,0 +1,38 @@
+//
+// routes.js
+//
+// - maps responses from Rails' UsersController to Paloma.Controller.AuthorsController
+//
+// by default:
+//  Rails controller will be mapped to a corresponding Paloma controller with the same name.
+//
+//
+Paloma.Route.resource('Users', {controller: 'Authors'});
+Paloma.Route.redirect('Users#edit', {to: 'Authors#revise'});
+
+// With namespace
+Paloma.Route.resource('Admin.Users', {controller: 'Authors'});
+Paloma.Rotue.resource('Users', {controller: 'Admin.Authors'});
+
+
+
+//
+// authors_controller.js
+//
+// - create Paloma.Controller.AuthorsController
+// - returns the controller constructor
+//
+var controller = Paloma.Controller.register('Authors');
+
+controller.before_filter('test', {only: ['edit', 'new']});
+controller.after_filter('test', {except: ['edit', 'new']});
+
+
+controller.prototype.edit = function(){
+  this.params['test']; // params from Rails
+  // Do something here.
+};
+
+
+
+var adminAuthorController = Paloma.Controller.register('Admin.Authors');
