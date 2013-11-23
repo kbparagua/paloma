fb9665b05c965ce56c45e2030ee3a795cf7379ee
Ben D'Angelo <ben@viafoura.com>
2013-11-23 17:40:49 -0500

    Didn't realize Paloma.env is null at that point

--- vendor/assets/javascripts/paloma/init.js
+++ vendor/assets/javascripts/paloma/init.js
@@ -3,13 +3,15 @@ window.Paloma = window.Paloma || {};
 //
 // Do nothing if there is no available console.
 //
-if (!window['console'] || Paloma.env != 'development'){ Paloma.log = Paloma.warn = function(msg){}; }
+if (!window['console']){ Paloma.log = Paloma.warn = function(msg){}; }
 else {
   Paloma.warn = function(msg){
+    if(Paloma.env != 'development') return;
     console.warn(msg);
   };
 
   Paloma.log = function(msg){
+    if(Paloma.env != 'development') return;
     console.log(msg);
   };
 }
