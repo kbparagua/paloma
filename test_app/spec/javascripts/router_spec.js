02f4f8c04b8d49ed8f68546641d8c5f7558d0171
kbparagua <kb.paragua@gmail.com>
2014-02-15 15:11:27 +0800

    Adjust router.js spec

--- test_app/spec/javascripts/router_spec.js
+++ test_app/spec/javascripts/router_spec.js
@@ -1,6 +1,6 @@
 
 describe('Paloma.Router', function(){
-  var delimiter = {namespace: '/', action: '#'},
+  var delimiter = '/',
       router = new Paloma.Router(delimiter);
 
   describe('#parse(path)', function(){
@@ -9,7 +9,7 @@ describe('Paloma.Router', function(){
       var result;
 
       beforeEach(function(){
-        result = router.parse('Foo/Bar/Baz/Controller#action');
+        result = router.parse('Foo/Bar/Baz/Controller');
       });
 
       it('returns the array of the namespaces', function(){
@@ -18,10 +18,6 @@ describe('Paloma.Router', function(){
 
       it('returns the controller', function(){
         expect(result.controller).toEqual('Controller');
-      });
-
-      it('returns the action', function(){
-        expect(result.action).toEqual('action');
       });
 
       it('returns the controllerPath', function(){
@@ -35,7 +31,7 @@ describe('Paloma.Router', function(){
       var result;
 
       beforeEach(function(){
-        result = router.parse('Controller#action');
+        result = router.parse('Controller');
       });
 
       it('returns an empty array of namespaces', function(){
@@ -46,73 +42,8 @@ describe('Paloma.Router', function(){
         expect(result.controller).toEqual('Controller');
       });
 
-      it('returns the action', function(){
-        expect(result.action).toEqual('action');
-      });
-
       it('returns the controllerPath', function(){
         expect(result.controllerPath).toEqual(['Controller']);
-      });
-    });
-
-  });
-
-
-
-
-
-  describe('#controllerFor(resource)', function(){
-
-    var resource = 'MySuperResource';
-
-    describe('when no route is found', function(){
-      it('returns the resource', function(){
-        var controller = router.controllerFor(resource);
-        expect(controller).toEqual(resource);
-      });
-    });
-
-
-    describe('when route is found', function(){
-      it('returns the set controller', function(){
-        router.resource(resource, {controller: 'MyController'});
-
-        var controller = router.controllerFor(resource);
-        expect(controller).toEqual('MyController');
-      });
-    });
-  });
-
-
-
-
-
-  describe('#redirectFor(resource, action)', function(){
-    var router = new Paloma.Router(delimiter),
-        result;
-
-    describe('when has a redirect', function(){
-      beforeEach(function(){
-        router.redirect('Foo#edit', {to: 'Bar#revise'});
-        result = router.redirectFor('Foo', 'edit');
-      });
-
-      it('returns controller of the redirect', function(){
-        expect(result.controller).toEqual('Bar');
-      });
-
-      it('returns action of the redirect', function(){
-        expect(result.action).toEqual('revise');
-      });
-    });
-
-
-    describe('when has no redirect', function(){
-      var router = new Paloma.Router(delimiter),
-          result = router.redirectFor('Foo', 'edit');
-
-      it('returns null', function(){
-        expect(result).toBeNull();
       });
     });
 
